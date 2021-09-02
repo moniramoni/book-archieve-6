166289e8078dcc3801342c92d94a6ca2b2c0a9fb
moniramoni <moniinfo60@gmail.com>
2021-09-02 17:36:39 +0600

    add

--- js/main.js
+++ js/main.js
@@ -1,56 +1,46 @@
 // Load books data
 const loadBooks = () => {
-    const searchField = document.getElementById('search-input');
+    // search input
+    const searchField = document.getElementById('search-input');   
     const searchText = searchField.value;
-    console.log(searchText);
     searchField.value = '';
 
-    fetch('https://openlibrary.org/search.json?q=javascript')
+    // api load
+    const url = `https://openlibrary.org/search.json?q=${searchText}`;
+    fetch(url)
     .then(res => res.json())
     .then(data => displayBooks(data.docs))
-    // .then(data => console.log(data.docs[0]))
-    // .then(data => console.log(data.docs[0].publish_date[1])) date year
-    // .then(data => console.log(data.docs[0].publish_date[0])) //pub date
-    // .then(data => console.log(data.docs[0].title)) // title
+
 }
+
 loadBooks()
 
 // Display books data 
 const displayBooks = books =>{
     console.log(books);
     const displayBook = document.getElementById('display-book-container');
+    // founded search results
+    const searchResultFound = document.getElementById('founded-search-result');
+    // searchResultFound.innerText = `Total Search results found = ${numFound}`
 
-/*     books.forEach(book => {
-        console.log(book); // ${book.cover_i}
-        const div = document.createElement('div');
-        div.classList.add('col');
-        div.innerHTML = `
-        <div class="card h-100 shadow p-0 border-0">
-            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" height="220px" class="card-img-top" alt="">
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
-            </div>
-        </div>
-        `
-        displayBook.appendChild(div)
-    });  */
-
-
+    // forEACH
     books.forEach(book => {
-        console.log(book); // ${book.cover_i}
+        console.log(book);
+
         const div = document.createElement('div');
         div.classList.add('col');
         div.innerHTML = `
         <div class="card h-100 shadow p-0 border">
             <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" height="220px" class="card-img-top" alt="">
             <div class="card-body">
-                <h5 class="card-title">${book.title}</h5>
-                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+                <h5 class="card-title fw-bold fs-2">${book.title}</h5>
+                <p class="card-text fst-italic fs-6"><strong class="fst-italic fs-6">Author:</strong> ${book.author_name?book.author_name: 'Unknown author name'}</p>
+                <p class="card-text fst-italic fs-6"><strong class="fst-italic fs-6">Publisher:</strong> ${book.publisher[0]}</p>
+                <p class="card-text fst-italic fs-6"><strong class="fst-italic fs-6">1st Publish Year:</strong> ${book.first_publish_year?book.first_publish_year: 'Unknown Year'}</p>
             </div>
         </div>
         `
-        displayBook.appendChild(div)
+        displayBook.appendChild(div);
     }); 
 }
 
