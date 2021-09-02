7f9ba8cb1e84acfe92499596f68de734c092a13f
moniramoni <moniinfo60@gmail.com>
2021-09-02 09:54:09 +0600

    search-input

--- js/main.js
+++ js/main.js
@@ -1,1 +1,24 @@
-console.log('hi');
+const loadBooks = () => {
+    fetch('https://openlibrary.org/search.json?q=javascript')
+    .then(res => res.json())
+    .then(data => console.log(data.docs))
+    // .then(data => console.log(data.docs[0]))
+    // .then(data => console.log(data.docs[0].publish_date[1])) date year
+    // .then(data => console.log(data.docs[0].publish_date[0])) //pub date
+    // .then(data => console.log(data.docs[0].title)) // title
+}
+loadBooks()
+
+/* 
+const displayBooks = books =>{
+    const searchText = document.getElementById('search-input');
+    const search = searchText.value;
+    console.log(search);
+
+    books.forEach(book => {
+        console.log(book);
+    });
+}
+loadBooks()
+
+ */
