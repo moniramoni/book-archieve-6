849bec2a5d52f075eb922620a187fa4644b08dd9
moniramoni <moniinfo60@gmail.com>
2021-09-02 11:57:22 +0600

    added

--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
+// Load books data
 const loadBooks = () => {
+    const searchField = document.getElementById('search-input');
+    const searchText = searchField.value;
+    console.log(searchText);
+    searchField.value = '';
+
     fetch('https://openlibrary.org/search.json?q=javascript')
     .then(res => res.json())
-    .then(data => console.log(data.docs))
+    .then(data => displayBooks(data.docs))
     // .then(data => console.log(data.docs[0]))
     // .then(data => console.log(data.docs[0].publish_date[1])) date year
     // .then(data => console.log(data.docs[0].publish_date[0])) //pub date
@@ -9,16 +15,49 @@ const loadBooks = () => {
 }
 loadBooks()
 
-/* 
+// Display books data 
 const displayBooks = books =>{
-    const searchText = document.getElementById('search-input');
-    const search = searchText.value;
-    console.log(search);
+    console.log(books);
+    const displayBook = document.getElementById('display-book-container');
+
+/*     books.forEach(book => {
+        console.log(book); // ${book.cover_i}
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100 shadow p-0 border-0">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-L.jpg" height="220px" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">Card title</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `
+        displayBook.appendChild(div)
+    });  */
+
 
     books.forEach(book => {
-        console.log(book);
-    });
+        console.log(book); // ${book.cover_i}
+        const div = document.createElement('div');
+        div.classList.add('col');
+        div.innerHTML = `
+        <div class="card h-100 shadow p-0 border">
+            <img src="https://covers.openlibrary.org/b/id/${book.cover_i}-M.jpg" height="220px" class="card-img-top" alt="">
+            <div class="card-body">
+                <h5 class="card-title">${book.title}</h5>
+                <p class="card-text">This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+            </div>
+        </div>
+        `
+        displayBook.appendChild(div)
+    }); 
 }
-loadBooks()
 
- */
+
+
+
+
+
+
+
